32c8a9793ca1236e12cea6bb420b496083798710
Ivaniuk Roman <romka2001ivaniuk@gmail.com>
2021-01-13 15:41:52 +0200

    add project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.future').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("flash");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.future future-beach').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("flash");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.future future-rocket').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("flash");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDownBig");
+	        }
+	    });
+	});
+})
